4b80be3cedd38c0d545c95ebe0be9d85de36ffcd
Moulali2004 <moulalisk2004@gmail.com>
2023-10-14 12:29:46 +0530

    Created a java solution for Leaders in an array (function)

---
+++ JAVA/Leaders.java
@@ -0,0 +1,20 @@
+/**
+ * Leaders
+ */
+class Leaders{
+    //Function to find the leaders in the array.
+    static ArrayList<Integer> leaders(int arr[], int n){
+        // Your code here
+        ArrayList<Integer> ans = new ArrayList<Integer>();
+        int lead = 0;
+        for(int i=n-1;i>=0;i--)
+        {
+            if(arr[i] >= lead){
+                lead = arr[i];
+                ans.add(arr[i]);
+            }
+        }
+        Collections.sort(ans, Collections.reverseOrder());
+        return ans;
+    }
+}
