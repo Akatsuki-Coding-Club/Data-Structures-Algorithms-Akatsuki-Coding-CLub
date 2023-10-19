123dd51dccc39c7e08f20d616ecdb7df7951ee9e
alpesh677 <bariaalpesh477@gmail.com>
2023-10-19 22:57:47 +0530

    Added heap sort in java

---
+++ JAVA/HeapSort.java
@@ -0,0 +1,81 @@
+import java.util.Scanner;
+
+public class HeapSort {
+    public static void heapSort(int arr[]) {
+        int n = arr.length;
+
+        // Build a max heap
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(arr, n, i);
+        }
+
+        // Extract elements one by one from the heap
+        for (int i = n - 1; i >= 0; i--) {
+            // Swap the root of the heap with the last element
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            // Call heapify on the reduced heap
+            heapify(arr, i, 0);
+        }
+    }
+
+    public static void heapify(int arr[], int n, int i) {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        // If the left child is larger than the root
+        if (left < n && arr[left] > arr[largest]) {
+            largest = left;
+        }
+
+        // If the right child is larger than the root
+        if (right < n && arr[right] > arr[largest]) {
+            largest = right;
+        }
+
+        // If the largest is not the root
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+
+            // Recursively heapify the affected sub-tree
+            heapify(arr, n, largest);
+        }
+    }
+
+    public static void printArray(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int size = scanner.nextInt();
+
+        int arr[] = new int[size];
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < size; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        System.out.println("Original array:");
+        printArray(arr);
+
+        heapSort(arr);
+
+        System.out.println("Sorted array:");
+        printArray(arr);
+
+        scanner.close();
+    }
+}
