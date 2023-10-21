1dfbaa357cf5670785823f7c2f14eaf8de9ea770
Samihan <samihannandedkar15@gmail.com>
2023-10-21 22:48:30 +0530

    fixed issue:#55 merge sort

---
+++ PYTHON/MergeSort.py
@@ -0,0 +1,34 @@
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left_half = arr[:mid]
+    right_half = arr[mid:]
+
+    left_half = merge_sort(left_half)
+    right_half = merge_sort(right_half)
+
+    return merge(left_half, right_half)
+
+def merge(left, right):
+    result = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    result.extend(left[i:])
+    result.extend(right[j:])
+    return result
+
+if __name__ == "__main__":
+    n = int(input())
+    arr = list(map(int,input().split(" ")))[:n]
+    sorted_arr = merge_sort(arr)
+    print(sorted_arr)
