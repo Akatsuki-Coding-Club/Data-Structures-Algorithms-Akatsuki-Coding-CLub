fe9f557e5892adc7407c1567b612d4c3660a757b
Harsh76200 <harshjain76200@gmail.com>
2023-10-22 01:44:25 -0700

    Added solution for Cyclically rotate problem

---
+++ JAVA/cyclically_rotate.java
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            int a[] = new int[n];
+            // long getAnswer[] = new long[(int)(n)];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(inputLine[i]);
+            }
+            
+            Compute obj = new Compute();
+            obj.rotate(a, n);
+            
+            StringBuilder output = new StringBuilder();
+            for(int i=0;i<n;i++)
+                output.append(a[i]+" ");
+            System.out.println(output);
+            
+        }
+	}
+}
+
+
+
+
+class Compute {
+    
+    public void rotate(int arr[], int n)
+    {
+        int ele=arr[n-1];
+        
+        for(int i = arr.length-1 ; i >0 ;i--)
+        {
+            arr[i]=arr[i-1];
+        }
+        arr[0]=ele;
+        
+    }
+}
